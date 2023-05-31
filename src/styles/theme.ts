2118d9ddbd2e073fff58c5199ad0bdba38d5e0a8
Juliana Chagas <julianavrchagas@gmail.com>
2023-05-31 13:00:23 -0300

    feat: add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  colors: {
+    white: '#fff',
+    primary: '#0d0d0f',
+    secondary: '#1a1a1a',
+    gray: '#737171',
+    accent: '#e50914',
+    yellow: '#f5c518',
+    white_70: 'rgba(255,255,255,0.7)',
+    separator: '#242429',
+  },
+  breakpoints: {
+    large: '1450px',
+    medium: '1200px',
+    small: '550px',
+  },
+} as const;

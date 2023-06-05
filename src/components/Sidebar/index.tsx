cef53dbd87a0ee407d825e933c5a35b0f4dde56e
Juliana Chagas <julianavrchagas@gmail.com>
2023-06-05 13:55:05 -0300

    feat: add sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,96 @@
+import { NavLink, useLocation } from 'react-router-dom';
+import { SidebarContainer } from './styles';
+import { Star, Heart, Popcorn, ChartLineUp } from 'phosphor-react';
+import React from 'react';
+import { GlobalContext } from '../../contexts/GlobalContext';
+import TmdbLogo from '../../assets/tmdb-logo.svg';
+import { BiCameraMovie } from 'react-icons/bi';
+
+export const Sidebar = () => {
+  const { mobileMenu, setMobileMenu } = React.useContext(GlobalContext);
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname, setMobileMenu]);
+
+  return (
+    <SidebarContainer className={mobileMenu ? 'active' : ''}>
+      <div className="logo">
+        <NavLink to="/movies/popular">
+          <BiCameraMovie />
+          <span>MovieLibrary</span>
+        </NavLink>
+      </div>
+      <nav>
+        <p>Movies</p>
+        <ul>
+          <li>
+            <NavLink to="/movies/popular">
+              <Heart />
+              <span>Popular</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/top_rated">
+              <Star />
+              <span>Top Rated</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/now_playing">
+              <Popcorn />
+              <span>Now Playing</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/upcoming">
+              <ChartLineUp />
+              <span>Upcoming</span>
+            </NavLink>
+          </li>
+        </ul>
+        <p>TV Series</p>
+        <ul>
+          <li>
+            <NavLink to="/tvseries/popular">
+              <Heart />
+              <span>Popular</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvseries/top_rated">
+              <Star />
+              <span>Top Rated</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvseries/airing_today">
+              <Popcorn />
+              <span>Airing Today</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvseries/on_the_air">
+              <ChartLineUp />
+              <span>Airing Soon</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <footer>
+        <a href="https://www.themoviedb.org/" target="_blank">
+          <img src={TmdbLogo} alt="" />
+        </a>
+        <span></span>
+        <p>
+          Copyright &copy;{' '}
+          <a href="https://github.com/julianachagas" target="_blank">
+            Juliana Chagas
+          </a>
+        </p>
+        <p>2023</p>
+      </footer>
+    </SidebarContainer>
+  );
+};
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+export const SidebarContainer = styled.article`
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 2;
+  width: 250px;
+  min-height: 100vh;
+  height: 100%;
+  border-right: 1px solid ${props => props.theme.colors.separator};
+  width: 250px;
+  background-color: ${props => props.theme.colors.primary};
+  transition: transform 0.3s ease, visibility 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  gap: 4.5rem;
+  padding: 4.5rem 3rem 0.5rem;
+  overflow-y: auto;
+  .logo {
+    a {
+      display: flex;
+      align-items: center;
+      gap: 0.8rem;
+    }
+
+    span {
+      font-size: 2.2rem;
+      font-weight: 600;
+    }
+    svg {
+      font-size: 3rem;
+      color: ${props => props.theme.colors.accent};
+    }
+  }
+
+  nav p {
+    font-size: 1.8rem;
+    font-weight: 600;
+    margin-bottom: 1.6rem;
+    &:nth-of-type(2) {
+      margin-top: 3.6rem;
+    }
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    a {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      color: ${props => props.theme.colors.white_70};
+      &.active {
+        color: ${props => props.theme.colors.white};
+        svg {
+          color: ${props => props.theme.colors.accent};
+        }
+      }
+      svg {
+        font-size: 2.2rem;
+      }
+    }
+    li:nth-child(1),
+    li:nth-child(2) {
+      a.active svg > path {
+        fill: ${props => props.theme.colors.accent};
+        stroke: ${props => props.theme.colors.accent};
+      }
+    }
+  }
+
+  footer {
+    margin-top: auto;
+    text-align: center;
+    line-height: 1.5;
+    p {
+      font-size: 1.4rem;
+      color: ${props => props.theme.colors.gray};
+    }
+
+    img {
+      width: 135px;
+      display: block;
+      margin: 0 auto;
+      margin-bottom: 1rem;
+    }
+
+    a:hover {
+      text-decoration: underline;
+      color: ${props => props.theme.colors.white};
+    }
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.medium}) {
+    z-index: initial;
+    .logo {
+      display: none;
+    }
+    transform: translateX(-100%);
+    visibility: hidden;
+    border-right: none;
+    padding-top: 10rem;
+    background-color: ${props => props.theme.colors.secondary};
+    width: 100%;
+    &.active {
+      transform: translateX(0%);
+      visibility: visible;
+    }
+  }
+`;

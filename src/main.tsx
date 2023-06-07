6fc4b601082815846cd47a29735520746b1407f1
Juliana Chagas <julianavrchagas@gmail.com>
2023-06-07 13:18:41 -0300

    feat: add global context

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './routes/Router.tsx';
+import { GlobalProvider } from './contexts/GlobalContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router />
+      <GlobalProvider>
+        <Router />
+      </GlobalProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );

6901d2ccafb52699f76e2dd03d2950b273a85e71
Juliana Chagas <julianavrchagas@gmail.com>
2023-06-29 14:02:26 -0300

    feat: add item component

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,108 @@
+import { useNavigate } from 'react-router-dom';
+import { ItemContainer } from './styles';
+import { ChartLineUp, Star, TelevisionSimple } from 'phosphor-react';
+import { ArrowLeft, Link, Calendar, Clock } from 'phosphor-react';
+import { ItemData } from '../../helpers/normalizeData';
+import { Poster } from '../Poster';
+import { StyledButton } from '../../styles/global';
+
+export function Item(props: ItemData) {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
+  return (
+    <ItemContainer>
+      <div className="content">
+        <Poster
+          poster={props.poster}
+          alt={props.title ? props.title : ''}
+          single={true}
+        />
+        <div>
+          <div className="rating">
+            <Star weight="fill" />
+            <span>{props.rating === '0.0' ? '-' : props.rating}</span>
+          </div>
+          <h1>{props.title}</h1>
+          {props.releaseDate && (
+            <p>{new Date(props.releaseDate).getFullYear().toString()}</p>
+          )}
+          <p className="genres">
+            {props.genres.map(genre => (
+              <span key={genre.id}>{genre.name}</span>
+            ))}
+          </p>
+          <p className="overview">{props.overview}</p>
+          {props.runtime && (
+            <p className="stats">
+              <Clock />
+              <span>Runtime: </span>
+              <span>{props.runtime} min</span>
+            </p>
+          )}
+          {props.seasons && (
+            <p className="stats">
+              <TelevisionSimple />
+              <span>Seasons:</span>
+              <span>{props.seasons}</span>
+              <span>| Episodes:</span>
+              <span>{props.episodes}</span>
+            </p>
+          )}
+          {props.status && (
+            <p className="stats">
+              <ChartLineUp />
+              <span>Status:</span>
+              <span>{props.status}</span>
+            </p>
+          )}
+          <p className="stats">
+            <Calendar />
+            <span>Release Date: </span>
+            <span>
+              {props.releaseDate
+                ? new Date(props.releaseDate).toLocaleDateString()
+                : '-'}
+            </span>
+          </p>
+          {props.lastAirDate && (
+            <p className="stats">
+              <Calendar />
+              <span>Last Air Date: </span>
+              <span>{new Date(props.lastAirDate).toLocaleDateString()}</span>
+            </p>
+          )}
+          <div className="links-wrapper">
+            {props.homepage && (
+              <a
+                href={props.homepage}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Link />
+                Website
+              </a>
+            )}
+            {props.imdb && (
+              <a
+                href={`https://www.imdb.com/title/${props.imdb}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Link />
+                IMDb
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+      <StyledButton onClick={goBack}>
+        <ArrowLeft />
+        Back
+      </StyledButton>
+    </ItemContainer>
+  );
+}
---
+++ src/components/Item/styles.ts
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+
+export const ItemContainer = styled.div`
+  margin-bottom: 5rem;
+  h1 {
+    margin-block: 1rem;
+  }
+
+  .content {
+    display: grid;
+    grid-template-columns: 342px 1fr;
+    gap: 5rem;
+    align-items: center;
+    margin-bottom: 4rem;
+    .rating {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      font-size: 1.5rem;
+      color: ${props => props.theme.colors.white_70};
+      svg {
+        color: ${props => props.theme.colors.yellow};
+        font-size: 1.6rem;
+      }
+    }
+
+    p:first-of-type,
+    .genres {
+      color: ${props => props.theme.colors.white_70};
+      font-size: 1.5rem;
+      line-height: 1.5;
+    }
+
+    .genres span:not(:last-child)::after {
+      content: ' | ';
+    }
+
+    .overview {
+      margin-block: 2rem;
+      line-height: 1.3;
+      text-align: justify;
+    }
+
+    .stats {
+      line-height: 1.8;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      span:first-of-type {
+        font-weight: 500;
+      }
+      svg {
+        font-size: 2rem;
+        color: ${props => props.theme.colors.accent};
+      }
+    }
+
+    .links-wrapper {
+      display: flex;
+      margin-block: 3rem;
+      gap: 2rem;
+      a {
+        font-size: 1.3rem;
+        background-color: transparent;
+        transition: all 0.3s ease-in-out;
+        border: 1px solid ${props => props.theme.colors.white};
+        padding: 0.8rem 2rem;
+        max-width: initial;
+        margin: initial;
+        border-radius: 50px;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.8rem;
+        svg {
+          font-size: 1.6rem;
+        }
+        &:focus-visible {
+          background-color: ${props => props.theme.colors.white};
+          color: ${props => props.theme.colors.primary};
+        }
+        &:hover:not(:focus-visible) {
+          background-color: ${props => props.theme.colors.gray};
+          color: ${props => props.theme.colors.white};
+          border-color: transparent;
+          transform: translateY(-3px);
+        }
+      }
+    }
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.medium}) {
+    margin-bottom: 0;
+    .content {
+      gap: 4rem;
+    }
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.small}) {
+    .content {
+      margin-bottom: 0;
+      grid-template-columns: 1fr;
+      grid-template-rows: 450px auto;
+      div:first-child {
+        max-width: 300px;
+        justify-self: center;
+      }
+    }
+  }
+`;
